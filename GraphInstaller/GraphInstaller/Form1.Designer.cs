3788b44e1e5d8cb47ce5e154b9b2aac1a0e9de99
matinlotfali <matinlotfali@gmail.com>
2014-09-27 11:41:44 +0330

    GraphInstaller Added

---
+++ GraphInstaller/GraphInstaller/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace GraphInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(123, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 94);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(347, 231);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(170, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(123, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Create a new project";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 39);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(155, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Install on an existing project";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(218, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "<Project Name>";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(303, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "C# Project|*.csproj";
+            this.openFileDialog1.Title = "Open C# Project";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 337);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Graph Emulator Installer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ GraphInstaller/GraphInstaller/Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace GraphInstaller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void WriteLog(string s)
+        {
+            richTextBox1.Text += s;
+            richTextBox1.Update();
+        }
+
+        bool vs2010 = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader reader = null;
+            StreamWriter writer = null;
+            try
+            {
+                richTextBox1.Text = "";
+                WriteLog("Checking the installation....");
+                string csprojFile;
+                string ProjectsDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Visual Studio 2008\\Projects\\";
+                if (!Directory.Exists(ProjectsDir))
+                {
+                    ProjectsDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Visual Studio 2010\\Projects\\";
+                    if (!Directory.Exists(ProjectsDir))
+                        throw new Exception("Can not find the projects directory.");
+                    vs2010 = true;
+                }
+                bool install = true;
+                DialogResult r;
+                if (Directory.Exists(ProjectsDir + textBox2.Text))
+                {
+                    r = MessageBox.Show("The project name already exists!\nDo you want to install GraphDLL on it without creating a new one?", "Confirmation", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                    if (r == DialogResult.Yes)
+                        install = false;
+                    else if (r == DialogResult.Cancel)
+                    {
+                        WriteLog("\n\n[Installation Canceled!]");
+                        return;
+                    }
+                }
+                if (radioButton1.Checked)
+                {
+                    if (install)
+                        CreateProject(textBox2.Text, ProjectsDir + textBox2.Text + "\\");
+                    csprojFile = ProjectsDir + textBox2.Text + "\\" + textBox2.Text + "\\" + textBox2.Text + ".csproj";
+                }
+                else
+                    csprojFile = textBox1.Text;
+                FileInfo csprojinfo = new FileInfo(csprojFile);
+                FileInfo exeinfo = new FileInfo(Application.ExecutablePath);
+                string ProgramFile = csprojinfo.Directory + "\\Program.cs";
+                string GraphDLLFile = exeinfo.Directory + "\\GraphDLL.dll";
+                string WMPLibFile = exeinfo.Directory + "\\Interop.WMPLib.dll";
+                string GraphDLLVersion = FileVersionInfo.GetVersionInfo(GraphDLLFile).FileVersion;
+                if (!install)
+                    WriteLog("[Done]\n");
+                WriteLog("Opening csproj file....");
+                reader = new StreamReader(csprojFile);
+                writer = new StreamWriter("temp.txt");
+                WriteLog("[Done]\n");
+
+                string lines;
+                while (!reader.EndOfStream)
+                {
+                    lines = reader.ReadLine();
+                    writer.WriteLine(lines);
+                    if (lines == "  <ItemGroup>")
+                        break;
+                }
+                if (reader.EndOfStream) throw new FileLoadException("The file is illegual for the installation.");
+
+                WriteLog("Checking the references....\n");
+                bool WinForm = false, Drawing = false, GraphDLL = false;
+                while (!reader.EndOfStream)
+                {
+                    lines = reader.ReadLine();
+                    if (lines == "    <Reference Include=" + '"' + "System.Drawing" + '"' + " />")
+                    {
+                        Drawing = true;
+                        WriteLog("\tSystem.Drawing found!\n");
+                    }
+                    else if (lines == "    <Reference Include=" + '"' + "System.Windows.Forms" + '"' + " />")
+                    {
+                        WinForm = true;
+                        WriteLog("\tSystem.Windows.Forms found!\n");
+                    }
+                    else if (lines == "    <Reference Include=" + '"' + "GraphDLL, Version=2.0.4.3, Culture=neutral, processorArchitecture=MSIL" + '"' + ">")
+                    {
+                        GraphDLL = true;
+                        WriteLog("\tGraphDLL found!\n");
+                    }
+                    else if (lines == "  </ItemGroup>")
+                        break;
+
+                    writer.WriteLine(lines);
+                }
+                if (reader.EndOfStream) throw new FileLoadException("The file is illegual for the installation.");
+
+                if (!GraphDLL)
+                {
+                    writer.WriteLine("    <Reference Include=" + '"' + "GraphDLL, Version=" + GraphDLLVersion + ", Culture=neutral, processorArchitecture=MSIL" + '"' + ">");
+                    writer.WriteLine("      <SpecificVersion>False</SpecificVersion>");
+                    writer.WriteLine("      <HintPath>" + GraphDLLFile + " \\GraphDLL.dll</HintPath>");
+                    writer.WriteLine("    </Reference>");
+                    WriteLog("\tGraphDLL added!\n");
+                }
+                if (!Drawing)
+                {
+                    writer.WriteLine("    <Reference Include=" + '"' + "System.Drawing" + '"' + " />");
+                    WriteLog("\tSystem.Drawing added!\n");
+                }
+                if (!WinForm)
+                {
+                    writer.WriteLine("    <Reference Include=" + '"' + "System.Windows.Forms" + '"' + " />");
+                    WriteLog("\tSystem.Windows.Forms added!\n");
+                }
+                writer.WriteLine("  </ItemGroup>");
+
+                if (!WinForm || !Drawing || !GraphDLL)
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        lines = reader.ReadLine();
+                        writer.WriteLine(lines);
+                    }
+                    reader.Close();
+                    writer.Close();
+                    File.Copy(csprojFile, csprojFile + ".bak", true);
+                    File.Copy("temp.txt", csprojFile, true);
+                }
+                else
+                {
+                    reader.Close();
+                    writer.Close();
+                }
+
+                WriteLog("Opening Program.cs file....");
+                reader = new StreamReader(ProgramFile);
+                writer = new StreamWriter("temp.txt");
+                WriteLog("[Done]\n");
+                WriteLog("Adding using....");
+                writer.WriteLine("using GraphDLL;");
+                writer.WriteLine("using System.Drawing;");
+                writer.WriteLine("using System.Windows.Forms;");
+                while (!reader.EndOfStream)
+                {
+                    lines = reader.ReadLine();
+                    if (lines != "using GraphDLL;"
+                            && lines != "using System.Drawing;"
+                            && lines != "using System.Windows.Forms;")
+                        writer.WriteLine(lines);
+                }
+                reader.Close();
+                writer.Close();
+                File.Copy(ProgramFile, ProgramFile + ".bak", true);
+                File.Copy("temp.txt", ProgramFile, true);
+                WriteLog("[Done]\n");
+
+                WriteLog("Copying GraphDLL.dll (v" + GraphDLLVersion + ")...");
+                Directory.CreateDirectory(csprojinfo.Directory + "\\bin\\Debug");
+                File.Copy(GraphDLLFile, csprojinfo.Directory + "\\bin\\Debug\\GraphDLL.dll", true);
+                WriteLog("[Done]\n");
+
+                WriteLog("Copying Interop.WMPLib.dll...");
+                File.Copy(WMPLibFile, csprojinfo.Directory + "\\bin\\Debug\\Interop.WMPLib.dll", true);
+                WriteLog("[Done]\n");
+                File.Delete("temp.txt");
+
+                WriteLog("\n[Installation Successfull]");
+
+                r = MessageBox.Show("Installation was successfull!\nDo you want to open the project?", "Installation Successfull", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (r == DialogResult.Yes)
+                {
+                    Process.Start(csprojFile);
+                    Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLog("\n\nError: " + ex.Message + "\nEmail me for help: matinlotfali@gmail.com");
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                    writer.Close();
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+                button1.Enabled = File.Exists(textBox1.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                button1.Enabled = (textBox2.Text.Length > 0) && (textBox2.Text[0] != '<');
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = !radioButton1.Checked;
+            button2.Enabled = !radioButton1.Checked;
+            textBox2.Enabled = radioButton1.Checked;
+            textBox2.Text = "<Project Name>";
+            textBox1_TextChanged(null, null);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult r = openFileDialog1.ShowDialog();
+            if (r == DialogResult.OK)
+                textBox1.Text = openFileDialog1.FileName;
+        }
+
+        void CreateProject(string name, string Path)
+        {
+            if (!vs2010)
+            {
+                WriteLog("[Done]\nCreating a new project...");
+                string file = Path + name + ".sln";
+                Directory.CreateDirectory(Path);
+                BinaryWriter binwriter = new BinaryWriter(new FileStream(file, FileMode.Create));
+                binwriter.Write(Properties.Resources.sln2008);
+                binwriter.Close();
+                ReplaceInFile("ConsoleApplication5", name, file);
+                WriteLog("\n\t" + name + ".sln installed!");
+
+
+                Directory.CreateDirectory(Path + name);
+                file = Path + name + "\\" + name + ".csproj";
+                binwriter = new BinaryWriter(new FileStream(file, FileMode.Create));
+                binwriter.Write(Properties.Resources.csproj2008);
+                binwriter.Close();
+                ReplaceInFile("ConsoleApplication5", name, file);
+                WriteLog("\n\t" + name + ".csproj installed!");
+
+                file = Path + name + "\\Program.cs";
+                binwriter = new BinaryWriter(new FileStream(file, FileMode.Create));
+                binwriter.Write(Properties.Resources.Program2008);
+                binwriter.Close();
+                ReplaceInFile("ConsoleApplication5", name, file);
+                WriteLog("\n\tProgram.cs installed!");
+
+                Directory.CreateDirectory(Path + name + "\\Properties");
+                file = Path + name + "\\Properties\\AssemblyInfo.cs";
+                binwriter = new BinaryWriter(new FileStream(file, FileMode.Create));
+                binwriter.Write(Properties.Resources.AssemblyInfo2008);
+                binwriter.Close();
+                ReplaceInFile("ConsoleApplication5", name, file);
+                WriteLog("\n\tAssemblyInfo.cs installed!\n");
+            }
+            else
+            {
+                WriteLog("[Done]\nCreating a new project...");
+                string file = Path + name + ".sln";
+                Directory.CreateDirectory(Path);
+                BinaryWriter binwriter = new BinaryWriter(new FileStream(file, FileMode.Create));
+                binwriter.Write(Properties.Resources.sln2010);
+                binwriter.Close();
+                ReplaceInFile("ConsoleApplication6", name, file);
+                WriteLog("\n\t" + name + ".sln installed!");
+
+
+                Directory.CreateDirectory(Path + name);
+                file = Path + name + "\\" + name + ".csproj";
+                binwriter = new BinaryWriter(new FileStream(file, FileMode.Create));
+                binwriter.Write(Properties.Resources.csproj2010);
+                binwriter.Close();
+                ReplaceInFile("ConsoleApplication6", name, file);
+                WriteLog("\n\t" + name + ".csproj installed!");
+
+                file = Path + name + "\\Program.cs";
+                binwriter = new BinaryWriter(new FileStream(file, FileMode.Create));
+                binwriter.Write(Properties.Resources.Program2010);
+                binwriter.Close();
+                ReplaceInFile("ConsoleApplication6", name, file);
+                WriteLog("\n\tProgram.cs installed!");
+
+                Directory.CreateDirectory(Path + name + "\\Properties");
+                file = Path + name + "\\Properties\\AssemblyInfo.cs";
+                binwriter = new BinaryWriter(new FileStream(file, FileMode.Create));
+                binwriter.Write(Properties.Resources.AssemblyInfo2010);
+                binwriter.Close();
+                ReplaceInFile("ConsoleApplication6", name, file);
+                WriteLog("\n\tAssemblyInfo.cs installed!\n");
+            }
+        }
+
+        void ReplaceInFile(string a, string b, string file)
+        {
+            StreamReader reader = new StreamReader(file);
+            StreamWriter writer = new StreamWriter("temp.txt");
+
+            string line = reader.ReadLine();
+            if (line.Length > 2)
+                line = line.Remove(0, 2);
+            line = line.Replace(a, b);
+            writer.WriteLine(line);
+            while (!reader.EndOfStream)
+            {
+                line = reader.ReadLine();
+                line = line.Replace(a, b);
+                writer.WriteLine(line);
+            }
+            reader.Close();
+            writer.Close();
+            File.Copy("temp.txt", file, true);
+        }
+    }
+}
